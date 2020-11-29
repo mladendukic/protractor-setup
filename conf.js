d038f287023aca26b4583f6a7c2d59aaa9a58722
Mladen <mladen.dukic@gmail.com>
2020-11-29 16:54:35 +0100

    Add jasmine reporter, remove dot reporter

--- conf.js
+++ conf.js
@@ -1,5 +1,19 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 exports.config = {
+
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['../protractor-setup/spec.js']
-}
+  specs: ['../protractor-setup/spec.js'],
+
+  jasmineNodeOpts: {
+    print: function () { } 
+  },
+
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }))
+  }
+};
